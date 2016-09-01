f7325657e537c3ec7fe90b4a83ac7c0d96cce7d1
jiyinyiyong <jiyinyiyong@gmail.com>
2016-09-01 20:39:41 +0800

    drop forced layout code; temporarily ignore composition event problem; bump 0.3.12

--- compiled/src/respo/render/patcher.cljs
+++ compiled/src/respo/render/patcher.cljs
@@ -16,20 +16,7 @@
 
 (defn replace-prop [target op]
   (let [prop-name (dashed->camel (name (key op))) prop-value (val op)]
-    (case
-      prop-name
-      "innerText"
-      (let [child-element-size (.-childElementCount target)]
-        (if (> child-element-size 0)
-          (.error
-            js/console
-            (str
-              "destroyed "
-              child-element-size
-              " elements during setting innerText!")))
-        (if (not= (.-innerText target) prop-value)
-          (set! (.-innerText target) prop-value)))
-      (aset target prop-name prop-value))))
+    (aset target prop-name prop-value)))
 
 (defn add-prop [target op]
   (let [prop-name (dashed->camel (name (key op))) prop-value (val op)]
@@ -37,16 +24,6 @@
       prop-name
       "style"
       (aset target prop-name (style->string prop-value))
-      "innerText"
-      (let [child-element-size (.-childElementCount target)]
-        (if (> child-element-size 0)
-          (.error
-            js/console
-            (str
-              "destroyed "
-              child-element-size
-              " elements during setting innerText!")))
-        (set! (.-innerText target) prop-value))
       (aset target prop-name prop-value))))
 
 (defn rm-prop [target op] (aset target (dashed->camel (name op)) nil))
