cccc4e513295887616aa6dbd28b6a6e78d89ea8c
jiyinyiyong <jiyinyiyong@gmail.com>
2017-11-27 00:50:01 +0800

    move main.cljs into coir.edn

---
+++ src/respo/main.cljs
@@ -0,0 +1,29 @@
+
+(ns respo.main
+  (:require [respo.core :refer [clear-cache! *changes-logger]]
+            [cljs.reader :refer [read-string]]
+            [respo.app.core :refer [render-app! *store]]))
+
+(def mount-target (.querySelector js/document ".app"))
+
+(defn main! []
+  (let [raw (.getItem js/window.localStorage "respo")]
+    (if (some? raw) (swap! *store assoc :tasks (read-string raw)))
+    (render-app! mount-target)
+    (add-watch *store :rerender #(render-app! mount-target))
+    (comment
+     reset!
+     *changes-logger
+     (fn [old-tree new-tree changes] (.log js/console (clj->js changes))))
+    (println "Loaded." (.now js/performance))))
+
+(defn save-store! [] (.setItem js/window.localStorage "respo" (pr-str (:tasks @*store))))
+
+(defn reload! []
+  (clear-cache!)
+  (render-app! mount-target)
+  (.log js/console "code updated."))
+
+(set! (.-onload js/window) main!)
+
+(set! (.-onbeforeunload js/window) save-store!)
